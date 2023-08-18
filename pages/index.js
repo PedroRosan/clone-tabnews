3f2a4c99e663080490c081c48707898df2223660
PedroRosan <pedro_rosan@DESKTOP-HDNDV8G>
2023-08-17 22:24:12 -0300

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return <h1>Bom dia meu amor, tudo bem? <br/> 
+    Eu te amo, estou voltando e você é capaz de fazer o mestrado! <br/>
+    Se você me ama da uma risadinha Tolipe.</h1>
+}
+
+export default Home;
